bc95b66061a29f0da1212a16497571dfb36fac47
stonk0105 <a0909726919@gmail.com>
2026-03-24 16:57:56 +0800

    toolbox

--- Toolbox.py
+++ Toolbox.py
@@ -13,13 +13,13 @@ def PA_N(df):
     return PA
 def K_N(df):
     k = len(df[df['PA_Result'].isin(['K', 'Ks', 'K-DO', 'K-BS', 'K-BF', 'K-DS','K-SF','K-P'])])
-    return k
+    return str(k)
 def BB_N(df):
     bb = len(df[df['PA_Result'].isin(['BB', 'BB-I', 'BB-IL', 'IBB', 'HBP','BB-P'])])
-    return bb
+    return str(bb)
 def H_N(df):
     hit = len(df[df["PA_Result"].isin(['1B', '2B', '3B', 'HR', 'IHR'])])
-    return hit
+    return str(hit)
 def AVG(df):
     hit = len(df[df["PA_Result"].isin(['1B', '2B', '3B', 'HR', 'IHR'])])
     AB = len(df[df['PA_Result'].isin(['1B', '2B', '3B', 'HR', 'IHR', 'DP', 'E-C', 'E-T', 'F', 'FC', 'FOT', 'G', 'G-', 'GT', 'IF', 'INT', 'K','K-BF','K-DO','K-BS', 'K-DS','Ks','K-SF','LO','K-P'])])
